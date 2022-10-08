1e813d26d193e388ef9afa7ce27fa8884f3d6d90
eastsage <dpwk8@naver.com>
2022-10-09 02:11:46 +0900

    MVC Framework -> SpringMVC Refactoring

--- src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -10,10 +10,10 @@ import java.util.Map;
 
 public class MemberListControllerV3 implements ControllerV3 {
 
-    private MemberRepository memberRepository = MemberRepository.getInstance();
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    public ModelView process(Map<String, String> paraMap) {
+    public ModelView process(Map<String, String> paramMap) {
         List<Member> members = memberRepository.findAll();
         ModelView mv = new ModelView("members");
         mv.getModel().put("members", members);
